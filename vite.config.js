ae797db9dbb241ee27553a89f789244f4f8cf710
Pramesh <prameshchamling2@gmail.com>
2025-11-28 09:13:41 +0545

    Banner Section for Naxa Service layout is done

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
   plugins: [tailwindcss()],
 });
